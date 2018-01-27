32111e70e65d384c47dea0d52f071c312069391d
Iago Lobo <iago.wolf.moraes@gmail.com>
2018-01-27 02:08:06 -0200

    Fazendo a parte do click rotacionar os pipes

---
+++ Assets/scripts/Pipe.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pipe : MonoBehaviour {
+
+	public enum PipeType {
+		straight,
+		curve,
+		doubleCurve,
+		empty,
+		plus
+	};
+	public PipeType pipeType;
+
+	public enum Direction{
+		UP,
+		RIGHT,
+		DOWN,
+		LEFT,
+		INVALID
+	};
+
+	private float angle = 0;
+	public float rotateSpeed;
+	private bool rotating = false;
+	private Direction lastDirection;
+
+	// Considere que a saída do cano gira no sentido horário, a partir da entrada
+	public Direction PipeOrientation(){
+		if (angle == 0)
+			return Direction.UP;
+		if (angle == 90)
+			return Direction.RIGHT;
+		if (angle == 180)
+			return Direction.DOWN;
+		if (angle == 270)
+			return Direction.LEFT;
+		
+		return Direction.INVALID;
+	}
+
+	public void Rotate()
+	{
+		lastDirection = PipeOrientation ();
+		rotating = true;
+		Debug.Log ("Rotate called!");
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button> ().onClick.Invoke ();
+//		Rotate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotating)
+		{
+			float deltaAngle = rotateSpeed * Time.deltaTime;
+			angle += deltaAngle;
+			Vector3 currentRotation = gameObject.transform.localRotation.eulerAngles;
+			float newAngle = currentRotation.z + deltaAngle;
+			Debug.Log (deltaAngle);
+			if (Mathf.CeilToInt (currentRotation.z) / 90 != Mathf.CeilToInt (newAngle) / 90) {
+				if (lastDirection == Direction.UP) {
+					angle = 90;
+				}
+				else if(lastDirection == Direction.RIGHT){
+					angle = 180;
+				}
+				else if(lastDirection == Direction.DOWN){
+					angle = 270;
+				}
+				else if(lastDirection == Direction.UP){
+					angle = 0;
+				}
+				gameObject.transform.rotation = Quaternion.Euler (currentRotation.x, currentRotation.y, angle);
+				rotating = false;
+			} else {
+				gameObject.transform.rotation = Quaternion.Euler (currentRotation.x, currentRotation.y, newAngle);
+			}
+		}
+	}
+}
